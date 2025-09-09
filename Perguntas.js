a04531866becf8f1645050221e0a8a9ac6b89757
Mazutti-J <jose.mazutti@escola.pr.gov.br>
2025-09-09 08:47:13 -0300

    perguntas

---
+++ Perguntas.js
@@ -0,0 +1,85 @@
+//1
+criaCartao(
+    'categoria', 
+    'Quem foi o primeiro presidente do Brasil?', 
+    'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca, que assumiu o cargo em 15 de novembro de 1889, após a Proclamação da República. Eleito por voto indireto em 25 de fevereiro de 1891, tornando-se o primeiro presidente eleito do país.'
+)
+//2
+criaCartao(
+    'categoria', 
+    'Qual é o país mais populoso do mundo?', 
+    'O país mais populoso do mundo é a Índia.'
+)
+//3
+criaCartao(
+    'categoria', 
+    'Quem é conhecido como o "Pai da Física"?', 
+    'O título de "pai da física" é frequentemente atribuído a Isaac Newton.'
+)
+//4
+criaCartao(
+    'categoria', 
+    'Qual é o maior oceano do mundo?', 
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+//5
+criaCartao(
+    'categoria', 
+    'De quem é a frase "Só sei que nada sei"?', 
+    'A famosa frase "Só sei que nada sei" é atribuída ao filósofo grego Sócrates.'
+)
+//6
+criaCartao(
+    'categoria', 
+    'Qual o único mamífero que voa?', 
+    'O único mamífero que voa de verdade é o morcego. Outros mamíferos, como os esquilos voadores, apenas planam, não possuem a capacidade de voar ativamente.'
+)
+//7
+criaCartao(
+    'categoria', 
+    'Quantos ossos tem um corpo humano adulto?', 
+    'O corpo humano adulto possui 206 ossos. No entanto, o número pode variar ligeiramente dependendo da fonte e de variações anatômicas individuais, como a presença de ossos acessórios.'
+)
+//8
+criaCartao(
+    'categoria', 
+    'O que significa utopia?', 
+    'lugar ou estado ideal, de completa felicidade e harmonia entre os indivíduos. Qualquer descrição imaginativa de uma sociedade ideal, fundamentada em leis justas e em instituições político-econômicas verdadeiramente comprometidas com o bem-estar da coletividade.'
+)
+//9
+criaCartao(
+    'categoria', 
+    'Como são chamadas as estátuas da Ilha de Páscoa?', 
+    'A Ilha de Páscoa inclui 900 estátuas monumentais chamadas moais.'
+)
+//10
+criaCartao(
+    'categoria', 
+    'O é sofisma?', 
+    'Argumento ou raciocínio concebido com o objetivo de produzir a ilusão da verdade, que, embora simule um acordo com as regras da lógica, apresenta, na realidade, uma estrutura interna inconsistente, incorreta e deliberadamente enganosa. Pressupõe a não correspondência biunívoca entre o que o indivíduo pensa e o que ele diz.'
+)
+//11
+criaCartao(
+    'categoria', 
+    'Quais são as primeiras quatro letras do alfabeto grego?', 
+    'Α (alfa), Β (beta), Γ (gama) e Δ (delta).'
+)
+//12
+criaCartao(
+    'categoria', 
+    'O que significa a palavra (do inglês) “push”?', 
+    'Empurrar.'
+)
+//13
+criaCartao(
+    'categoria', 
+    'Ao que se refere a palavra erudito?', 
+    'A pessoa que tem vontade de aprender sempre, que está sempre estudando, procurando se aprofundar em assuntos cultos, sobre a história do mundo, das artes, da música, etc. Na Bíblia, o termo "erudito" refere-se a alguém com conhecimento profundo e vasto, adquirido principalmente através da leitura.'
+)
+//14
+criaCartao( 
+    'categoria', 
+    'Qual é a última letra do alfabeto grego?', 
+    'A última letra do alfabeto grego é Ômega (Ω, ω).'
+)
+
