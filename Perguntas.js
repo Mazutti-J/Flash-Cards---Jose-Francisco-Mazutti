0b45d5faf191752a561719bc7b8c177ee1cf4256
Mazutti-J <jose.mazutti@escola.pr.gov.br>
2025-09-23 09:21:36 -0300

    ddd

--- Perguntas.js
+++ Perguntas.js
@@ -3,103 +3,103 @@ criaCartao(
     'Hitória', 
     'Quem foi o primeiro presidente do Brasil?', 
     'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca, que assumiu o cargo em 15 de novembro de 1889, após a Proclamação da República. Eleito por voto indireto em 25 de fevereiro de 1891, tornando-se o primeiro presidente eleito do país.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //2
 criaCartao(
     'Geografia', 
     'Qual é o país mais populoso do mundo?', 
     'O país mais populoso do mundo é a Índia.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //3
 criaCartao(
     'Física', 
     'Quem é conhecido como o "Pai da Física"?', 
     'O título de "pai da física" é frequentemente atribuído a Isaac Newton.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //4
 criaCartao(
     'Geografia', 
     'Qual é o maior oceano do mundo?', 
     'O maior oceano do mundo é o Oceano Pacífico.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //5
 criaCartao(
     'Filosofia', 
     'De quem é a frase "Só sei que nada sei"?', 
     'A famosa frase "Só sei que nada sei" é atribuída ao filósofo grego Sócrates.'.at,
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //6
 criaCartao(
     'Biologia', 
     'Qual o único mamífero que voa?', 
     'O único mamífero que voa de verdade é o morcego. Outros mamíferos, como os esquilos voadores, apenas planam, não possuem a capacidade de voar ativamente.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //7
 criaCartao(
     'Teologia', 
     'O que é a Páscoa?', 
     'A Páscoa é uma festa cristã que celebra a ressurreição de Jesus Cristo, ocorrida três dias após sua crucificação. É a celebração mais importante do cristianismo e marca o fim da Semana Santa, que é a última semana da Quaresma. Ovos e coelhos são símbolos comuns da Páscoa, com raízes em tradições pagãs que foram incorporadas ao cristianismo. ',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //8
 criaCartao(
     'Biologia', 
     'Quantos ossos tem um corpo humano adulto?', 
     'O corpo humano adulto possui 206 ossos. No entanto, o número pode variar ligeiramente dependendo da fonte e de variações anatômicas individuais, como a presença de ossos acessórios.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //9
 criaCartao(
     'Português', 
     'O que significa utopia?', 
     'lugar ou estado ideal, de completa felicidade e harmonia entre os indivíduos. Qualquer descrição imaginativa de uma sociedade ideal, fundamentada em leis justas e em instituições político-econômicas verdadeiramente comprometidas com o bem-estar da coletividade.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //10
 criaCartao(
     'História', 
     'Como são chamadas as estátuas da Ilha de Páscoa?', 
     'A Ilha de Páscoa inclui 900 estátuas monumentais chamadas moais.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //11
 criaCartao(
     'Português', 
     'O é sofisma?', 
     'Argumento ou raciocínio concebido com o objetivo de produzir a ilusão da verdade, que, embora simule um acordo com as regras da lógica, apresenta, na realidade, uma estrutura interna inconsistente, incorreta e deliberadamente enganosa. Pressupõe a não correspondência biunívoca entre o que o indivíduo pensa e o que ele diz.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //12
 criaCartao(
     'Grego', 
     'Quais são as primeiras quatro letras do alfabeto grego?', 
     'Α (alfa), Β (beta), Γ (gama) e Δ (delta).',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //13
 criaCartao(
     'Inglês', 
     'O que significa a palavra (do inglês) “push”?', 
     'Empurrar.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //14
 criaCartao(
     'Português', 
     'Ao que se refere a palavra erudito?', 
     'A pessoa que tem vontade de aprender sempre, que está sempre estudando, procurando se aprofundar em assuntos cultos, sobre a história do mundo, das artes, da música, etc. Na Bíblia, o termo "erudito" refere-se a alguém com conhecimento profundo e vasto, adquirido principalmente através da leitura.',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
 //15
 criaCartao( 
     'Grego', 
     'Qual é a última letra do alfabeto grego?', 
     'A última letra do alfabeto grego é Ômega (Ω, ω).',
-    'src="https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339"'
+    'https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png/revision/latest/smart/width/250/height/250?cb=20181217162339'
 )
